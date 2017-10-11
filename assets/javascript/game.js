7d6dfec7197aa4e873f10bd92c1c3d8f9ec02800
janetcushing <janet.cushing@gmail.com>
2017-10-11 16:16:33 -0400

    code is working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,35 @@
 // javascript for game
 
+
 //------------------//
-//global variables
+// JSON objects
+//------------------//
+var crystalObject = {
+    "blueNumber": 0,
+    "greenNumber": 0,
+    "redNumber": 0,
+    "purpleNumber": 0,
+    "crystalNumber": 0,
+    "totalNumber": 0
+};
+
+var trackGameObject = {
+    "isFirstTimeThrough": true,
+    "winCntr": 0,
+    "lossCntr": 0,
+    "winLoseMsg": ""
+};
+
+//------------------//
+//global variables 
 //------------------//
 
-// var crystalObject = {
-//     "blueNumber": 0,
-//     "greenNumber": 0,
-//     "redNumber": 0,
-//     "purpleNumber": 0,
-//     "crystalNumber": 0,
-//     "totalNumber": 0
-// };
-
-var blueNumber = 0;
-var greenNumber = 0;
-var redNumber = 0;
-var purpleNumber = 0;
-var crystalNumber = 0;
-var totalNumber = 0;
-var winLoseMsg = "";
-var beginAgainMsg = "Do you want to play again?";
-// var playAgain = true;
-var winCntr = 0;
-var loseCntr = 0;
-var isFirstTimeThrough = true;
+var lossCount = 0;
+var winCount = 0;
 var storedWinCntr;
 var storedLoseCntr;
 
-var playAgainHtml = '<div class="input-group">' +
-    '<div id="radioBtn" class="btn-group">' +
-    '<p>Do you want to play again?</p>' +
-    '<a class="btn btn-primary btn-sm active" id="yes" data-toggle="happy" data-title="Y" value="true">YES</a>' +
-    '<a href="gameOver.html" class="btn btn-primary btn-sm notActive" id="no" data-toggle="happy" ' + 'data-title="N" value="false">NO</a>' +
-    '</div>' +
-    '<input type="hidden" name="happy" id="happy">' +
-    '</div>';
+
 
 //---------------------------//
 // functions
@@ -45,173 +39,143 @@ function getRandomNumber(min, max) {
     return randomNum;
 }
 
-function beginGame() {
+
+// initialize json variables and intialize the DOM
+function beginGame(theTrackGameObject) {
+    theTrackGameObject.winLoseMsg = "";
+    theTrackGameObject.isFirstTimeThrough = false;
+    $('#winLoseMsg').text(theTrackGameObject.winLoseMsg);
+    $("#playAgain").hide();
+    return theTrackGameObject;
 
 }
 
+// initialize json crystalaNumber object and display to the DOM
+function initializeCrystalObject(theCrystalObject) {
+    theCrystalObject.blueNumber = getRandomNumber(1, 12);
+    theCrystalObject.greenNumber = getRandomNumber(1, 12);
+    theCrystalObject.redNumber = getRandomNumber(1, 12);
+    theCrystalObject.purpleNumber = getRandomNumber(1, 12);
+    theCrystalObject.crystalNumber = getRandomNumber(19, 120);
+    theCrystalObject.totalNumber = 0;
+    $('#crystal').text(theCrystalObject.crystalNumber);
+    $('#total').text(theCrystalObject.totalNumber);
+    return theCrystalObject;
+
+}
+
+// process a loss
+function processLoss(theTrackLossObject) {
+    theTrackLossObject.winLoseMsg = "You Lose!";
+    theTrackLossObject.lossCntr++;
+    console.log("theTrackLossObject.lossCntr" + theTrackLossObject.lossCntr);
+    theTrackLossObject = processWinOrLoss(theTrackLossObject);
+    return theTrackLossObject;
+}
+
+// process a win
+function processWin(theTrackWinObject) {
+    theTrackWinObject.winLoseMsg = "You Win!";
+    theTrackWinObject.winCntr++;
+    theTrackWinObject = processWinOrLoss(theTrackWinObject);
+    return theTrackWinObject;
+}
+
+// nested function is called from  processLoss and processWin functions
+function processWinOrLoss(theTrackWinOrLossObject) {
+    $('#winLoseMsg').html(theTrackWinOrLossObject.winLoseMsg);
+    $('#winCntr').html(theTrackWinOrLossObject.winCntr);
+    console.log("theTrackWinOrLossObject.lossCntr" + theTrackWinOrLossObject.lossCntr);
+    $('#lossCntr').html(theTrackWinOrLossObject.lossCntr);
+    $('#blue', '#red', '#green', '#purple').off('click');
+    $("#playAgain").show();
+    return theTrackWinOrLossObject;
+
+}
+
+// update the total and then update the total in the DOM
+function updateTotalCounter(theTotalNumber, theButtonNumber) {
+    theTotalNumber = theTotalNumber + theButtonNumber;
+    $('#total').html(theTotalNumber);
+    return theTotalNumber;
+}
+
+
+
 
 //----------------------//
-//main processing
+// main processing
 //----------------------//
 $(document).ready(function () {
-    console.log("isFirstTimeThrough" + isFirstTimeThrough);
 
-    if (isFirstTimeThrough) {
-        // initialize numbers and remove playAgain message
-        console.log("first time through");
-        blueNumber = getRandomNumber(1, 12);
-        greenNumber = getRandomNumber(1, 12);
-        redNumber = getRandomNumber(1, 12);
-        purpleNumber = getRandomNumber(1, 12);
-        crystalNumber = getRandomNumber(19, 120);
-        totalNumber = 0;
-        winLoseMsg = "";
-        $('#winLoseMsg').text(winLoseMsg);
-        $('#crystal').text(crystalNumber);
-        $('#total').text(totalNumber);
-        $("#playAgain").hide();
-        isFirstTimeThrough = false;
+    if (trackGameObject.isFirstTimeThrough) {
+        // initialize numbers and hide the play Again message
+        crystalObject = initializeCrystalObject(crystalObject);
+        trackGameObject = beginGame(trackGameObject);
     }
 
 
+    // clicking yes or no buttons determines if player wants to play again or not
     $("#yes").click(function () {
-        // initialize numbers and remove playAgain message
-        console.log("im in the yes fyunction");
-        blueNumber = getRandomNumber(1, 12);
-        greenNumber = getRandomNumber(1, 12);
-        redNumber = getRandomNumber(1, 12);
-        purpleNumber = getRandomNumber(1, 12);
-        crystalNumber = getRandomNumber(19, 120);
-        totalNumber = 0;
-        winLoseMsg = "";
-        $('#winLoseMsg').text(winLoseMsg);
-        $('#crystal').text(crystalNumber);
-        $('#total').text(totalNumber);
-        $("#playAgain").hide();
-
+        // initialize numbers and hides the playAgain message and buttons
+        crystalObject = initializeCrystalObject(crystalObject);
+        trackGameObject = beginGame(trackGameObject);
     });
 
     $("#no").click(function () {
+        // display the gameOver page
+    //    move the win and loss counters to the gameOver page
+        localStorage.winCounter  =  trackGameObject.winCntr;
+        localStorage.lossCounter = trackGameObject.lossCntr;
+        
+        $('#lossCntr').text(localStorage.lossCounter);
+        $('#winCntr').text(localStorage.winCounter);
         window.location = 'gameOver.html';
     });
 
+
+    // process a blue button click
     $('#blue').click(function () {
-        totalNumber = totalNumber + blueNumber;
-        $('#total').html(totalNumber);
-        if (parseInt(totalNumber) > parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').click(false);
-            winLoseMsg = "You Lose!";
-            loseCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#lossCntr').html(loseCntr);
-            // sstoredLossCntr = Number(SessionStorage.lossCntr)
-            $("#playAgain").show();
-            // $("#radioBtn").data("data-title", playAgain);
-            // $("#radioBtn").click(function () {
-            //     playAgain = $("#radioBtn").value;
-            // });
+        crystalObject.totalNumber = updateTotalCounter(crystalObject.totalNumber, crystalObject.blueNumber);
+        if (parseInt(crystalObject.totalNumber) > parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processLoss(trackGameObject);
 
-        } else if (parseInt(totalNumber) === parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').click(false);
-            winLoseMsg = "You Win!";
-            winCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#winCntr').html(winCntr);
-            $("#playAgain").show();
-            // $("#radioBtn").data("data-title", playAgain);
-            // $("#radioBtn").click(function () {
-            //     playAgain = $("#radioBtn").value;
-            // });
+        } else if (parseInt(crystalObject.totalNumber) === parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processWin(trackGameObject);
         }
-        console.log("playAgain" + playAgain);
     });
 
+    // process a green button click
     $('#green').click(function () {
-        totalNumber = totalNumber + greenNumber;
-        $('#total').html(totalNumber);
-        if (parseInt(totalNumber) > parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').off('click');
-            winLoseMsg = "You Lose!";
-            loseCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#lossCntr').html(loseCntr);
-            $("#playAgain").show();
-            // $("#radioBtn").data("data-title", playAgain);
-            $("#radioBtn").click(function () {
-                playAgain = $("#radioBtn").value;
-            });
-        } else if (parseInt(totalNumber) === parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').off('click');
-            winLoseMsg = "You Win!";
-            winCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#winCntr').html(winCntr);
-            $("#playAgain").show();
-            // $("#radioBtn").data("data-title", playAgain);
-            // $("#radioBtn").click(function () {
-            //     playAgain = $("#radioBtn").value;
-            // });
+        crystalObject.totalNumber = updateTotalCounter(crystalObject.totalNumber, crystalObject.greenNumber);
+        if (parseInt(crystalObject.totalNumber) > parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processLoss(trackGameObject);
+
+        } else if (parseInt(crystalObject.totalNumber) === parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processWin(trackGameObject);
         }
     });
 
+    // process a red button click
     $('#red').click(function () {
-        totalNumber = totalNumber + redNumber;
-        $('#total').html(totalNumber);
-        if (parseInt(totalNumber) > parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').off('click');
-            winLoseMsg = "You Lose!";
-            loseCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#lossCntr').html(loseCntr);
-            $("#playAgain").show();
-            // $("#radioBtn").data("data-title", playAgain);
-            // $("#radioBtn").click(function () {
-            //     playAgain = $("#radioBtn").value;
-            // });
-        } else if (parseInt(totalNumber) === parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').off('click');
-            winLoseMsg = "You Win!";
-            winCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#winCntr').html(winCntr);
-            $("#playAgain").show();
-            // $("#radioBtn").data("value", playAgain);
-            // $("#radioBtn").click(function () {
-            //     playAgain = $("#radioBtn").value;
-            // });
+        crystalObject.totalNumber = updateTotalCounter(crystalObject.totalNumber, crystalObject.redNumber);
+        if (parseInt(crystalObject.totalNumber) > parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processLoss(trackGameObject);
+
+        } else if (parseInt(crystalObject.totalNumber) === parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processWin(trackGameObject);
         }
     });
 
-
+    // process a purple button click
     $('#purple').click(function () {
-        totalNumber = totalNumber + purpleNumber;
-        $('#total').html(totalNumber);
-        if (parseInt(totalNumber) > parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').off('click');
-            winLoseMsg = "You Lose!";
-            loseCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#lossCntr').html(loseCntr);
-            $("#playAgain").show();
-            // $("#radioBtn").data("data-title", playAgain);
-            // $("#radioBtn").click(function () {
-            //     playAgain = $("#radioBtn").value;
-            // });
-        } else if (parseInt(totalNumber) === parseInt(crystalNumber)) {
-            $('#blue', '#red', '#green', '#purple').off('click');
-            winLoseMsg = "You Win!";
-            winCntr++;
-            $('#winLoseMsg').html(winLoseMsg);
-            $('#winCntr').html(winCntr);
-            $("#playAgain").show();
-            // $("#playAgain").append(playAgainHtml);
-            // $("#radioBtn").data("data-title", playAgain);
-            // $("#radioBtn").click(function () {
-            //     playAgain = $("#radioBtn").value;
-            // });
+        crystalObject.totalNumber = updateTotalCounter(crystalObject.totalNumber, crystalObject.purpleNumber);
+        if (parseInt(crystalObject.totalNumber) > parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processLoss(trackGameObject);
+
+        } else if (parseInt(crystalObject.totalNumber) === parseInt(crystalObject.crystalNumber)) {
+            trackGameObject = processWin(trackGameObject);
         }
     });
 
-
-    // $("#playAgain").on("click", <a>, function(){blah})
 });
